d0e3ef7a3a41c020f0e55c48ab18a722aadca8ee
Sasha <ponomarek91@gmail.com>
2020-02-08 00:22:36 +0200

    done

---
+++ 04-Homework/hw4.js
@@ -0,0 +1,62 @@
+const students = ["Саша","Игорь","Лена","Ира","Алексей","Светлана"];
+const themes = ["Дифференциальные уравнения", "Теория автоматов", "Алгоритмы и структуры данных"];
+const marks = [4, 5, 5, 3, 4, 5];
+
+let copyStudents = students.slice(0,);
+let buff;
+buff=copyStudents[1];
+copyStudents[1]=copyStudents[2];
+copyStudents[2]=buff;
+function makePairs(names){
+    const result =[];
+    for (let i=1;i<names.length;i++) {
+        if(i%2!==0){
+            result.push(names.slice(i-1,i+1));
+        }
+    }
+    return result;
+}
+const pairs = makePairs(copyStudents);
+console.log(`Розбиття на пари: \n`,pairs);
+
+function addThemes(projects,groups){
+    groups[0].splice(0,2,'Саша и Лена');
+    groups[1].splice(0,2,"Игорь и Ира");
+    groups[2].splice(0,2,"Алексей и Светлана");
+    for(let i = 0;i<groups.length;i++){
+        groups[i].push(projects[i]);
+    }
+    return groups;
+}
+
+let pairsCopy1 = makePairs(copyStudents);
+const pairsWithThemes = addThemes(themes,pairsCopy1);
+console.log("Пари з темою: \n",pairsWithThemes);
+
+function addMarks(names,value){
+    const result = [];
+    let temp=[];
+    for (let i=0;i<names.length;i++) {
+        temp.push(names[i]);
+        result.push(temp);
+        result[i].push(value[i]);
+        temp=[];
+    }
+    return result;
+}
+const studentWithMarks = addMarks(students,marks);
+console.log("Студенти з оцінкою: \n",studentWithMarks);
+
+function addRandomMarksToPairs(groups){
+    const result = [];
+    let temp=[];
+    for(let i=0;i<groups.length;i++) {
+        temp.push(groups[i][0],groups[i][1],Math.ceil(Math.random()*5));
+        result.push(temp);
+        temp=[];
+    }
+    return result;
+}
+
+const pairsWithMarks = addRandomMarksToPairs(pairsWithThemes);
+console.log("Пари з темою і оцінкою: \n",pairsWithMarks);
